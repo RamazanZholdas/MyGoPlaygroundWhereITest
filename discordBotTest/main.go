16751ba5bebaac918df3f9e729ff030fb62b2989
Zholdas Ramazan <ghostcomrad@gmail.com>
2022-03-06 00:43:47 +0600

    second commit

--- discordBotTest/main.go
+++ discordBotTest/main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"os"
+
+	translate "github.com/RamazanZholdas/testingFlagsHere/translateRussian"
 
 	"github.com/gocolly/colly"
-	"github.com/gocolly/colly/extensions"
 )
 
 /*https://www.kinopoisk.ru/index.php?level=7&from=forma&result=adv&m_act%5Bfrom%5D=forma&m_act
@@ -22,6 +20,48 @@ https://www.kinopoisk.ru/index.php?level=7&from=forma&result=adv&m_act%5Bfrom%5D
 &m_act%5Bactor%5D=Dylan&m_act%5Bcast%5D=Roy&m_act%5Bcontent_find%5D=serial&m_act%5Bgenre_and%5D=on
 */
 
+type SelectOption struct {
+	Id   []string `json:"id"`
+	Name []string `json:"name"`
+}
+
+func main() {
+	year := "2018"
+	country := "США"
+	id, err := translate.CheckCountry(country)
+	if err != nil {
+		log.Fatal(err)
+	}
+	movie, rating := "Dallas", ""
+	name, nextUrl, timing, info := "", "", "", ""
+	VisitUrl := "https://www.kinopoisk.ru/index.php?level=7&from=forma&result=adv&m_act%5Bfrom%5D=forma&m_act%5Bwhat%5D=content&m_act%5Bfind%5D=" + movie + "&m_act%5Byear%5D=" + year + "&m_act%5Bcountry%5D=" + id
+	fmt.Println(VisitUrl)
+	c := colly.NewCollector()
+
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println("We are visiting this site:", r.Request.URL)
+	})
+
+	c.OnHTML(".element.most_wanted", func(h *colly.HTMLElement) {
+		name = h.ChildText(".info>p.name")
+		timing = h.ChildText("span:nth-child(2)")
+		info = h.ChildText("span:nth-child(3)")
+		rating = h.ChildText(".right>div")
+		nextUrl = h.ChildAttr("a", "href")
+		nextUrl = "https://www.kinopoisk.ru" + nextUrl[0:len(nextUrl)-5]
+	})
+
+	c.Visit(VisitUrl)
+
+	fmt.Println(name, "\n", timing[4:], "\n", info, "\n", rating)
+	fmt.Println("for detailed information u can visit this site:", nextUrl)
+}
+
+/*
 func main() {
 	c := colly.NewCollector()
 	//countries := make(map[string]string)
@@ -71,41 +111,5 @@ func main() {
 
 type Country struct {
 	Name []string `json:"name"`
-}
-
-/*
-func main() {
-	suggested := []string{}
-	movie, rating, urlToVisit := "Dallas", "", ""
-	name, nextUrl, timing, info := "", "", "", ""
-	VisitUrl := "https://www.kinopoisk.ru/index.php?level=7&from=forma&result=adv&m_act%5Bfrom%5D=forma&m_act%5Bwhat%5D=content&m_act%5Bfind%5D=" + movie
-	c := colly.NewCollector()
-
-	c.OnError(func(r *colly.Response, err error) {
-		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
-	})
-
-	c.OnResponse(func(r *colly.Response) {
-		fmt.Println("We are visiting this site:", r.Request.URL)
-	})
-
-	c.OnHTML(".element.most_wanted", func(h *colly.HTMLElement) {
-		name = h.ChildText(".info>p.name")
-		timing = h.ChildText("span:nth-child(2)")
-		info = h.ChildText("span:nth-child(3)")
-		rating = h.ChildText(".right>div")
-		nextUrl = h.ChildAttr("a", "href")
-		nextUrl = "https://www.kinopoisk.ru" + nextUrl[0:len(nextUrl)-5]
-	})
-
-	c.OnHTML(".search_results search_results_last>p.header", func(h *colly.HTMLElement) {
-		urlToVisit = "https://www.kinopoisk.ru" + h.ChildAttr("a", "href")
-	})
-
-	c.Visit(VisitUrl)
-
-	fmt.Println(name, "\n", timing[4:], "\n", info, "\n", rating)
-	fmt.Println("for detailed information u can visit this site:", nextUrl)
-	fmt.Println(suggested, urlToVisit)
 }
 */
--- discordBotTest/translateRussian/translate.go
+++ discordBotTest/translateRussian/translate.go
@@ -1,10 +1,21 @@
 package translate
 
+import (
+	"encoding/json"
+	"errors"
+	"os"
+)
+
 var (
 	cipher   = []string{}
 	cirillic = []rune("йцукенгшщзхъфывапролджэячсмитьбю")
 	decode   = "E9F6F3EAE5EDE3F8F9E7F5FAF4FBE2E0EFF0EEEBE4E6FDFFF7F1ECE8F2FCE1FE"
 )
+
+type SelectOption struct {
+	Id   []string `json:"id"`
+	Name []string `json:"name"`
+}
 
 func initialize() {
 	for i := 0; i < len(decode); i += 2 {
@@ -23,4 +34,26 @@ func Translating(str string) string {
 		}
 	}
 	return res
+}
+
+func CheckCountry(str string) (string, error) {
+	byteSlice, err := os.ReadFile("./Country.json")
+	if err != nil {
+		return "", err
+	}
+
+	data := SelectOption{}
+
+	err = json.Unmarshal(byteSlice, &data)
+	if err != nil {
+		return "", err
+	}
+
+	for i := range data.Name {
+		if data.Name[i] == str {
+			return data.Id[i], nil
+		}
+	}
+
+	return "", errors.New("country does not exist")
 }
